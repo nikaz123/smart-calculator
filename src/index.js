153d4e69cb3933eef8475080f37a4b9a6af77417
Lenovo <nikaz@tut.by>
2018-03-06 23:58:35 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,27 +1,109 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.result = initialValue;
+        this.operation = [];
+        this.operation.push(initialValue);
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        this.operation.push(`+`);
+        this.operation.push(number);
 
-  devide(number) {
-    // your implementation
-  }
+        return this;
 
-  pow(number) {
-    // your implementation
-  }
+    }
+
+    subtract(number) {
+        this.operation.push("-");
+        this.operation.push(number);
+
+        return this;
+    }
+
+    multiply(number) {
+        this.operation.push("*");
+        this.operation.push(number);
+
+        return this;
+    }
+
+    devide(number) {
+        this.operation.push("/");
+        this.operation.push(number);
+
+        return this;
+    }
+
+    pow(number) {
+        this.operation.push("^");
+        this.operation.push(number);
+
+        return this;
+    }
+
+    valueOf() {
+
+        if (this.operation.length == 0) return this.result;
+
+
+        while (this.operation.lastIndexOf("^") != -1) {
+
+            let a = this.operation.lastIndexOf("^");  ///индекс знакак ^
+
+            let powResult = Math.pow(this.operation[a - 1], this.operation[a + 1]);
+
+            this.operation.splice(a - 1, 3, powResult);
+
+        }
+
+
+        while (this.operation.indexOf("*") != -1) {
+
+            let a = this.operation.indexOf("*");  ///индекс знакак *
+
+            let multResult = this.operation[a - 1] * this.operation[a + 1];
+
+            this.operation.splice(a - 1, 3, multResult);
+
+        }
+
+
+        while (this.operation.indexOf("/") != -1) {
+
+            let a = this.operation.indexOf("/");  ///индекс знакак *
+
+            let divResult = this.operation[a - 1] / this.operation[a + 1];
+
+            this.operation.splice(a - 1, 3, divResult);
+
+        }
+        while (this.operation.indexOf("-") != -1) {
+
+            let a = this.operation.indexOf("-");  ///индекс знакак *
+
+            let subsResult = this.operation[a - 1] - this.operation[a + 1];
+
+            this.operation.splice(a - 1, 3, subsResult);
+
+        }
+        while (this.operation.indexOf("+") != -1) {
+
+            let a = this.operation.indexOf("+");  ///индекс знакак *
+
+            let sumResult = this.operation[a - 1] + this.operation[a + 1];
+
+            this.operation.splice(a - 1, 3, sumResult);
+
+        }
+
+
+
+        this.result = this.operation[0];
+
+
+        return this.result;
+    }
 }
 
 module.exports = SmartCalculator;
